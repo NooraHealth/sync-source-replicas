4ddaef43fdf2ec58f07babc41bc56bc383246f67
Rahul M <rahulm@NH-PLC-LT-0008.local>
2025-06-26 14:49:09 +0530

    Initial commit

---
+++ googledrive.R
@@ -0,0 +1,71 @@
+
+# Dependencies ------------------------------------------------------------
+library(googledrive)
+
+SERVICE_ACCOUNT_PATH='secrets/service_account_creds.json'
+GDRIVE_FOLDER_ID='1khmWpbSCnFawvIq8Yg7Xus3QbDDJdL5D'
+# Authorization -----------------------------------------------------------
+noora_google_auth <-
+  function(authorize=TRUE){
+    if(!file.exists(SERVICE_ACCOUNT_PATH)){
+      log_error('Service account credentials not found. Exiting...')
+      return(NULL)
+    } else {
+        NULL
+    }
+    if(authorize==TRUE){
+      googledrive::drive_auth(path = SERVICE_ACCOUNT_PATH)
+      log_info('Googledrive API successfully authorized!')
+    } else {
+      googledrive::drive_deauth()
+      log_info('Googledrive API successfully deauthorized')
+    }
+  }
+
+noora_gdrive_folder_validate <-
+  function(id=GDRIVE_FOLDER_ID){
+    on.exit(noora_google_auth(FALSE))
+
+    noora_google_auth(TRUE)
+
+    tryCatch({
+      folder_info <- drive_get(as_id(id))
+      return(nrow(folder_info) > 0)
+    }, error = function(e) {
+      log_fatal('Non-valid folder details provided. Please verify. Error: ', e$message)
+      return(FALSE)
+    })
+  }
+
+noora_gdrive_folder_init <-
+  function(id=GDRIVE_FOLDER_ID){
+    folder_id=as_id(id)
+    folders_to_be_created=c('raw','latest')
+    is_valid_folder=noora_gdrive_folder_validate(id)
+    if(is_valid_folder==TRUE){
+      NULL
+    } else {
+      log_error('Cannot initialize folder due to non-valid folder id')
+      return(NULL)
+    }
+    # Check if folder is empty or not
+    noora_google_auth(authorize = TRUE)
+    if(nrow(drive_ls(as_id(GDRIVE_FOLDER_ID)))==0){
+      log_info('Folder tagged is empty. Primed for init')
+      NULL
+    } else {
+      log_error('Folder not empty! Unable to initialize!')
+      return(NULL)
+    }
+    # Get folder structure ready
+    for (folder in folders_to_be_created) {
+      drive_mkdir(name = folder,path = folder_id)
+    }
+    log_info('Folders created successfully!')
+    # Create Control Panel Spreadsheet
+    drive_create(name = "SurveyCTO Control Panel",
+                path = folder_id,
+                type = "spreadsheet")
+    log_info('Control Panel Successfully Created!')
+    noora_google_auth(authorize = FALSE)
+  }
---
+++ surveycto.R
@@ -0,0 +1,94 @@
+
+# Dependencies ------------------------------------------------------------
+library(rsurveycto)
+library(logger)
+library(dplyr)
+
+# Logging config ----------------------------------------------------------
+logger::log_threshold(level = DEBUG)
+
+# Secrets -----------------------------------------------------------------
+SURVEYCTO_SECRETS_FILE_PATH='secrets/scto_auth.txt'
+FORM_DEFINITION_DIRECTORY='form_definition_repo'
+
+# Relevant Methods --------------------------------------------------------
+noora_scto_auth_key <-
+  # Function to access the authorization key for rsurveyCTO.
+  function(){
+    if (file.exists(SURVEYCTO_SECRETS_FILE_PATH)) {
+      tryCatch(
+        expr = {auth <- rsurveycto::scto_auth(auth_file = SURVEYCTO_SECRETS_FILE_PATH)},
+        error = function(e){log_fatal(conditionMessage(e))}
+      )
+      log_info('SurvyCTO authorization successful.')
+      return(auth)
+    } else {
+      log_fatal('SurveyCTO authorization failed.')
+      return(NULL)
+    }
+  }
+
+
+noora_scto_catalog <-
+  # Function to access global catalog stored in surveycto server
+  function(){
+    authorization <-  noora_scto_auth_key()
+    if(is.null(authorization)) {
+      log_error('Unable to extract SCTO catalog')
+      break
+    } else {
+      catalog = rsurveycto::scto_catalog(authorization)
+      log_info('SurveyCTO catalog successfully saved.')
+      return(catalog)
+    }
+  }
+
+noora_scto_form_definition_download <-
+  # Downloads all form definitions in their raw format and stores it in
+  # a designated folder. Also returns a data.table with some basic information of the form  definition downloaded.
+  function(form_id=NULL){
+    auth=noora_scto_auth_key()
+    if(!dir.exists(FORM_DEFINITION_DIRECTORY)){
+      log_info('Folder does not exist. Creating...')
+      dir.create(FORM_DEFINITION_DIRECTORY)
+    } else {
+      log_info('Folder exists. Deleting all existing contents...')
+      unlink(file.path(FORM_DEFINITION_DIRECTORY, "*"), recursive = TRUE)
+      log_info('Contents cleared!')
+    }
+    if(is.null(auth)) {
+      log_error('Unable to access metadata information, due to invalid auth')
+      return(NULL)
+    } else {
+      raw_metadata=rsurveycto::scto_get_form_metadata(auth = auth,
+                                                  form_ids = form_id,
+                                                  deployed_only = TRUE,
+                                                  def_dir = FORM_DEFINITION_DIRECTORY,
+                                                  get_defs = TRUE)
+      cleaned_metadata=raw_metadata |>
+        dplyr::select(form_id,form_version,date_str,
+                      actor) |>
+        dplyr::rename(c('form_deployed_date'='date_str',
+                        'user_ac_who_deployed_form'='actor')) |>
+        dplyr::mutate(donloaded_file_name=
+                        stringr::str_c(form_id,'__',form_version,'.xlsx')
+                      )
+      return(cleaned_metadata)
+    }
+  }
+
+noora_scto_update <-
+  function(form_id=NULL){
+    catalog=noora_scto_catalog() |>
+      filter(is_deployed==TRUE & type=="form") |>
+      select(group_title,title,id,form_version,num_submissions_complete) |>
+      distinct() |> rename(c('form_id'='id'))
+    main=noora_scto_form_definition_download(form_id) |>
+      left_join(catalog,by = c('form_id','form_version')) |>
+      select(group_title,title,form_id,
+             form_version,num_submissions_complete,
+             form_deployed_date,user_ac_who_deployed_form,
+             downloaded_file_name)
+    return(main)
+  }
+
