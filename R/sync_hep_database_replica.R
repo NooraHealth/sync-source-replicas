64163aa52e705e4eefc6ab828e0437a36d4166db
Jake Hughey <jakejhughey@gmail.com>
2025-11-10 12:41:56 +0530

    update server datasets for hep facilities and trainers on surveycto.

--- R/sync_hep_database_replica.R
+++ R/sync_hep_database_replica.R
@@ -1,6 +1,9 @@
 source(here::here('R', 'utilities.R'))
 
-sync_hep_database_replica = \(con, query_path, file_url, folder_url) {
+sync_hep_database_replica = \(con, query_path, params) {
+
+  if (!is.null(params$scto_id_suffix)) auth = get_scto_auth()
+
   query_str = readChar(query_path, file.info(query_path)$size)
 
   rej = '-- sheet_name: '
@@ -10,20 +13,25 @@ sync_hep_database_replica = \(con, query_path, file_url, folder_url) {
 
   for (i in seq_len(length(queries))) {
     d = db_get_query(con, queries[i])
+    dataset_id = paste0('hep_', sheet_names[i])
 
-    if (!is.null(file_url)) {
-      sheet_write(d, ss = file_url, sheet = sheet_names[i])
+    if (!is.null(params$output_file_url)) {
+      sheet_write(d, ss = params$output_file_url, sheet = sheet_names[i])
     }
 
-    if (!is.null(folder_url)) {
-      media = here(tempdir(), paste0('hep_', sheet_names[i], '.csv'))
+    if (!is.null(params$output_folder_url)) {
+      media = here(tempdir(), paste0(dataset_id, '.csv'))
       fwrite(d, media)
-      drive_put(media, path = folder_url, name = basename(media))
+      drive_put(media, path = params$output_folder_url, name = dataset_id)
+    }
+
+    if (!is.null(params$scto_id_suffix)) {
+      scto_write(auth, d, paste0(dataset_id, params$scto_id_suffix))
     }
   }
 
-  d = data.table(synced_at = format(Sys.time(), '%FT%XZ', tz = 'GMT'))
-  sheet_write(d, ss = file_url, sheet = 'status')
+  status = data.table(synced_at = format(Sys.time(), '%FT%XZ', tz = 'GMT'))
+  sheet_write(status, ss = params$output_file_url, sheet = 'status')
 }
 
 
@@ -33,5 +41,4 @@ con = DBI::dbConnect(bigrquery::bigquery(), project = params$project)
 
 query_path = here('R', 'queries.sql')
 
-sync_hep_database_replica(
-  con, query_path, params$output_file_url, params$output_folder_url)
+sync_hep_database_replica(con, query_path, params)
